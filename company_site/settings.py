70fb8083e3f6b3d4090d6d9f8a1a768c26b65a85
nadaseylan05-pixel <nadaseylan05@gmail.com>
2026-04-24 16:08:56 +0300

    Update settings.py , desigadd whitenoise for production

--- company_site/settings.py
+++ company_site/settings.py
@@ -65,6 +65,7 @@ SITE_ID = 1
 # --------------------------------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware', #--> for the design of admin page
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -137,6 +138,8 @@ USE_TZ = True
 # --------------------------------------------------
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

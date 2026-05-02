5872e8b63d336834222d12d604f6589aef088348
nadaseylan05-pixel <nadaseylan05@gmail.com>
2026-05-02 14:38:06 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import home ,hakimizda,blog,ekibimiz,work_areas_view,blog,iletişim,kariyer, service_area_detail, blog_detail
+from .views import home ,hakimizda,blog,ekibimiz,work_areas_view,blog,iletişim,kariyer
 
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import  BlogPageSitemap,SiteContentSitemap, TeamPageSitemap, AboutPageSitemap, ServicesPageSitemap, KarierSitemap, ServiceAreaSitemap, BlogPostSitemap,
@@ -22,12 +22,12 @@ urlpatterns=[
     path('blog',blog,name='blog'),
     path('ekibimiz',ekibimiz,name='ekibimiz'),
     path('alanlarimiz/', work_areas_view, name='Çalışma_alanları'),
-    path("services/<int:id>/", service_area_detail, name="service_area_detail"),
+    #path("services/<int:id>/", service_area_detail, name="service_area_detail"),
     path('blog/', blog, name='blog'),
-    path("blog/<int:id>/", blog_detail, name="blog_detail"),
+    #path("blog/<int:id>/", blog_detail, name="blog_detail"),
     path('iletişim/', iletişim, name='iletişim'),
     #path('send_message/', send_message, name='send_message'),
     path('kariyer/', kariyer, name='kariyer'),
     #path('sitemaps.xml', sitemap, {'sitemaps':sitemap}, name='django.contrib.sitemaps.views.sitemaps'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),    
+    ##path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),    
 ]

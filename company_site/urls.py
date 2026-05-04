ae0b0f58ff99eb71e64157ad25c410c9bc14d5a8
nadaseylan05-pixel <nadaseylan05@gmail.com>
2026-05-04 17:31:07 +0300

    Update urls.py

--- company_site/urls.py
+++ company_site/urls.py
@@ -22,8 +22,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls')),
     path("google005df6b4842867af.html", google_verify),
+    path('',include('main.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

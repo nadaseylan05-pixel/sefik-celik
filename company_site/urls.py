2c281ff426f899cbf199eb3b19c48cb5acfe4c5c
nadaseylan05-pixel <nadaseylan05@gmail.com>
2026-05-04 17:29:18 +0300

    Update urls.py

--- company_site/urls.py
+++ company_site/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from main.views import google_verify
 from django.urls import path
 from django.urls import include
 from django.conf import settings
@@ -22,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
+    path("google005df6b4842867af.html", google_verify),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
